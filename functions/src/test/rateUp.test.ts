ae6dbfdae46718cfd814a5b38e1201a8ed48ffe6
Jerzy <jerzy.lasyk@consensys.net>
2018-12-17 21:22:43 +0100

    on enter hous pay reward

---
+++ functions/src/test/rateUp.test.ts
@@ -0,0 +1,33 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyDFnnKsggZqXDcZKHFjQGVd6PpnRiVicrw",
+    authDomain: "mojodating-prealpha.firebaseapp.com",
+    databaseURL: "https://mojodating-prealpha.firebaseio.com",
+    projectId: "mojodating-prealpha",
+    storageBucket: "mojodating-prealpha.appspot.com",
+    messagingSenderId: "617219767268"
+  };
+
+firebase.initializeApp(config);
+
+const email = "jerzy@test.com";
+const password = "test1234";
+
+console.log('test');
+
+firebase.functions
+firebase.auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(user => user.user.getIdToken())
+    .then(token => {
+        console.log('start execution');
+        const addMessage = firebase.functions().httpsCallable('rateUp');
+        addMessage({ uid: 'test' }).then(function(result) {
+            console.log('result: ', result);
+        })
+        .catch(err => { console.log('inner error: ', err); });
+    })
+    .catch(err => {
+        console.log('error: ', err);
+    });
